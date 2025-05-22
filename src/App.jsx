5bd1c7f3eaeefc958654471d133dc458d939320c
Virtus Dakura <dakuravirtus@gmail.com>
2025-05-22 10:20:03 +0000

    add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Service from './components/Service';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 
@@ -16,6 +17,7 @@ function App() {
         <Service />
         <Projects />
         <Contact />
+        <Footer />
 
       </div>
   )
